fc391b3799d2d89e8237f020ecf58e85f48bd22b
Albert Hwang <albert@eastmedia.com>
2010-05-14 18:02:33 -0400

    more layout stuffs...

--- sushi example/application.js
+++ sushi example/application.js
@@ -1,14 +1,36 @@
-var myResults = "";
+/*
+Objects
+
+Dataset[data]
+  datatype (support for string, numerical value, categories)
+x axis
+y axis
+radios
+axis bounds
+
+
+Description:
+left side is the y axis
+bottom is the x axis
+graph top right
+graph 400 tall, 600 wide
+
+
+
+
+*/
+
+var dataset = "";
 
 /* does this need a 'dynasortNum' appended: isn't the item index good enough? */
 $.getJSON(
   "http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20local.search%20where%20query%3D%22sushi%22%20and%20location%3D%22san%20francisco,%20ca%22&format=json&callback=",
   function(data){
-    myResults = data.query.results.Result;
-    $.each(myResults, function(i,item){
+    dataset = data.query.results.Result;
+    $.each(dataset, function(i,item){
       item.dynasortNum = i;
       var rowContainer = $("<div class='item' id='item_" + i + "' />");
-      rowContainer.appendTo("#items");
+      rowContainer.appendTo("#graph");
       $("<div/>").html(item.Title).appendTo("#item_" + i);
     });
   }
@@ -16,14 +38,14 @@ $.getJSON(
 
 $(document).ready(function () {
   $('#bn_sort_by_name_and_length').click(function() {
-    $.each(myResults, function(i,item){
+    $.each(dataset, function(i,item){
       sortByNameAndLength();
     });
     return false;
   });
 
   $('#bn_sort_by_rating_and_distance').click(function() {
-    $.each(myResults, function(i,item){
+    $.each(dataset, function(i,item){
       sortByRatingAndDistance();
     });
     return false;
@@ -32,7 +54,7 @@ $(document).ready(function () {
 });
 
 function sortByNameAndLength() {
-  $.each(myResults, function(i,item){
+  $.each(dataset, function(i,item){
     itemLeft = item.Title.length * 30;
     itemLeftInPix = itemLeft + "px";
     itemTop = item.Title.toUpperCase().charCodeAt() * 10 - 600;
@@ -42,7 +64,7 @@ function sortByNameAndLength() {
 }
 
 function sortByRatingAndDistance() {
-  $.each(myResults, function(i,item){
+  $.each(dataset, function(i,item){
     itemLeft = item.Distance * 100;
     itemLeftInPix = itemLeft + "px";
     itemTop = item.Rating.AverageRating * 100;
