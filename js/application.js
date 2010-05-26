783acbd00d52c2b4c07d41f2ddcac07263369714
U-DELLBERT\Phedhex <Phedhex@dellbert.(none)>
2010-05-26 01:43:46 -0400

    make json calls local

--- js/application.js
+++ js/application.js
@@ -27,10 +27,10 @@ var yLabel;
 $(document).ready(function(){
   graphHeight = pageHeight - 50;
   graphWidth = pageWidth - $('#control_pad').width() - 50;
-  $.getJSON(
+  //$.getJSON(
   //"http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20local.search%20where%20query%3D%22sushi%22%20and%20location%3D%22san%20francisco,%20ca%22&format=json&callback=",
-    "http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20local.search(0)%20where%20query%3D%22pizza%22%20and%20location%3D%22New%20york%2C%20ny%22&format=json&diagnostics=true&callback=",
-    function(data){
+  //"http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20local.search(0)%20where%20query%3D%22pizza%22%20and%20location%3D%22New%20york%2C%20ny%22&format=json&diagnostics=true&callback=",
+  //  function(data){
       dataset = data.query.results.Result;
       $.each(dataset, function(i,item){
         var rowContainer = $("<div class='item' id='item_" + i + "' />");
@@ -39,8 +39,8 @@ $(document).ready(function(){
         $("<div/>").html(item.Title).appendTo("#item_" + i);
       });
       drawGraph();
-    }
-  );
+  //  }
+  //);
   
   $('input:radio').change(function(){
     drawGraph();
